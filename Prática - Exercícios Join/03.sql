c06cc84d31a0847f2c5e21e5b123ee7eff92a7cd
Melissa <melmihomel@gmail.com>
2022-12-14 19:30:42 -0300

    feat: q3 from practice join

---
+++ Prática - Exercícios Join/03.sql
@@ -0,0 +1,1 @@
+SELECT users.id, users.name, courses.name AS "course", schools.name AS "school", educations."endDate" FROM users JOIN educations ON educations."userId" = users.id JOIN schools ON educations."schoolId" = schools.id JOIN courses ON educations."courseId" = courses.id WHERE educations.status = 'finished' AND users.id = 30;
